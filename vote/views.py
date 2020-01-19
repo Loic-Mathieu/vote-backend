9c95a4ae702390e74fa09d7fd22de7e2f80d91c3
Loic-Mathieu <loicn.mathieu@gmail.com>
2020-01-19 02:51:08 +0100

    correction permission_class

--- vote/views.py
+++ vote/views.py
@@ -13,17 +13,17 @@ from rest_framework.permissions import IsAuthenticated
 class TestViewSet(viewsets.ModelViewSet):
     queryset = Candidat.objects.all()
     serializer_class = TestSerializer
-    permission_classes = (IsAuthenticated)
+    permission_classes = (IsAuthenticated,)
 
 class VoteViewSet(viewsets.ModelViewSet):
     queryset = CandidatsNbVote.objects.all()
     serializer_class = VoteSerializer
-    permission_classes = (IsAuthenticated)
+    permission_classes = (IsAuthenticated,)
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = Etudiant.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsAuthenticated)
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         queryset = Etudiant.objects.all()
